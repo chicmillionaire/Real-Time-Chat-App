35f238015e212abcc66c738b8a4251edd3bbf025
chicmillionaire <chicmillionaire@gmail.com>
2020-05-19 00:06:43 -0400

    3 files modified

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,7 +1,22 @@
 import * as React from 'react';
+import * as ReactIs from 'react-is';
+import styled from 'styled-components';
+
+const Container = styled.div
+  display: flex;
+  flex-wrap: wrap;
+  width: 100vw;
+  height: 100vh;
+
+const SidebarContainer = styled.div
+  
 
 export function Layout () {
   return (
-    <div>test</div>
+    <Container>
+    <div>sidebar</div>
+    <div>message box</div>     
+    
+    </Container>
   )
 }
--- index.tsx
+++ index.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import { Layout } from './components/Layout';
+import { Layout } from './components/layout';
 import './style.css';
 
 interface AppProps { }
