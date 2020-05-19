a125b167355ee6733b359a8bfd084f45d2545b1f
chicmillionaire <chicmillionaire@gmail.com>
2020-05-18 22:32:02 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-n3slpg
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-n3slpg)
---
+++ components/layout.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export function Layout () {
+  return (
+    <div>test</div>
+  )
+}
